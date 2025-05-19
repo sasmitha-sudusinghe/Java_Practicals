00c7131c744fd6923589dd7356342feda9ab3d8a
Sasmitha_Sudusinghe <sasmithasudusinghe@gmail.com>
2025-05-19 11:08:29 +0530

    P2.java

--- Session 01/P2.java
+++ Session 01/P2.java
@@ -3,7 +3,7 @@
 
 import java.io.*;//this is how we import packeges
 
-class MySecondClass{//class name should start with capital letter
+class P2{//class name should start with capital letter
 	public static void main(String[] args){
 		System.out.println(args[0]+" "+args[2]+" "+args[1]);	
 		
