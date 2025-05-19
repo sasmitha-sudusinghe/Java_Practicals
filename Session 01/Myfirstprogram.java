68571509abd68ee396db74a89a81fca3557b6e9a
Sasmitha_Sudusinghe <sasmithasudusinghe@gmail.com>
2025-05-19 10:38:30 +0530

    Add files via upload

---
+++ Session 01/Myfirstprogram.java
@@ -0,0 +1,8 @@
+import java.io.*;
+class Myfirstprogram{
+	public static void main(String[] args){
+		System.out.print("Hi");
+		System.out.println("This is IT1214 Practical session 1");
+		System.out.println("This text will be on a new line");
+	}
+}
---
+++ Session 01/P2.java
@@ -0,0 +1,11 @@
+//this is a comment
+//this is our first java program
+
+import java.io.*;//this is how we import packeges
+
+class MySecondClass{//class name should start with capital letter
+	public static void main(String[] args){
+		System.out.println(args[0]+" "+args[2]+" "+args[1]);	
+		
+	}
+}
